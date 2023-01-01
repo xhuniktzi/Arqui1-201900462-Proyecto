56eaa55bfb1890cb5adb0c0b91ff171d4fc37986
Xhunik Miguel <xhuniktzi@gmail.com>
2023-01-01 13:28:40 -0600

    calculate coef

--- fputest.asm
+++ fputest.asm
@@ -1,8 +1,8 @@
 
 
 printAscii macro ascii
-               mov ax, @data    ; ax = offset de la cadena
-               mov ds, ax       ; ds = segmento de la cadena
+    ;    mov ax, @data    ; ax = offset de la cadena
+    ;    mov ds, ax       ; ds = segmento de la cadena
                mov ah, 02h      ; imprimir caracter
                mov dl, ascii    ; dx = offset de la cadena
                int 21h          ; imprimir cadena
@@ -11,15 +11,25 @@ endm
 .model small
 .stack 100h
 .data
-    txt      db "Hola ke ace"
-    num      dq 3.14159
-    const_10 dw 10
+    txt       db "Hola ke ace"
+    num       dq 0.00001
+    const_10  dw 10
 
-    integer  dw 0
+    integer   dw 0
 
-    config   dw 0C3Fh
+    config    dw 0C3Fh
 
-             
+    coefAWord dw 1
+    coefBWord dw 1
+    coefCWord dw 1
+    coefDWord dw 2
+    coefEWord dw -10
+    coefFWord dw -20
+
+    calc      dq 0
+    x         dq 0
+
+    wordTemp  dw 0
 .code
 
 
@@ -81,12 +91,72 @@ printInt proc
                    ret
 printInt endp
 
+calcValue proc
+                   
+                   fldz
+                   fstp       calc
+    ; x^5
+                   fld        x
+                   fmul       x
+                   fmul       x
+                   fmul       x
+                   fmul       x
+
+                   fimul      coefAWord
+                   fadd       calc
+                   fstp       calc
+    ; x^4
+                   fld        x
+                   fmul       x
+                   fmul       x
+                   fmul       x
+                   
+                   fimul      coefBWord
+                   fadd       calc
+                   fstp       calc
+    ; x^3
+                   fld        x
+                   fmul       x
+                   fmul       x
+                       
+                   fimul      coefCWord
+                   fadd       calc
+                   fstp       calc
+    ; x^2
+                   fld        x
+                   fmul       x
+                  
+                   fimul      coefDWord
+                   fadd       calc
+                   fstp       calc
+    ; x^1
+                   fld        x
+                   
+                   fimul      coefEWord
+                   fadd       calc
+                   fstp       calc
+    ; x^0
+                   fld        calc
+                   fiadd      coefFWord
+                   fstp       calc
+
+                   ret
+calcValue endp
+
 main proc
                    mov        ax, @data
                    mov        ds, ax
                    finit
                    fldcw      config
-                   
+
+                   fld        num
+                   fstp       x
+    
+                   call       calcValue
+
+                   fld        calc
+                   fstp       num
+
                    call       printFloat
 
                    mov        al, 0h
